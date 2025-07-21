e8230a80056fe90d4d5fa1ab0cd9c3ecd2507c90
Jain Waldrip <jain.waldrip@perforce.com>
2025-07-21 15:21:00 -0500

    Add Puppet Cert class

---
+++ lib/puppet_cert.rb
@@ -0,0 +1,28 @@
+# !/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Copyright 2025. Puppet, Inc., a Perforce company.  
+
+class PuppetCert  
+
+  CERT_DIR = "/etc/puppetlabs/puppet/ssl/certs/"
+
+  @certificate = nil; # OpenSSL::X509::Certificate
+
+  def initialize
+    @filename = get_filename  
+    raise "Certificate file #{@filename} does not exist" unless File.exist?(@filename)
+    raise "Certificate file #{@filename} is not readable" unless File.readable?(@filename)
+    raise "Certificate file #{@filename} is empty" unless File.size?(@filename) > 0
+    @certificate = OpenSSL::X509::Certificate.new(File.read(File.join(CERT_DIR, @filename)))
+  end
+
+  def get_filename
+    hostname = exec('hostname -f').strip.downcase
+    return CERT_DIR + "#{hostname}.pem"
+  end
+
+  def get_certificate
+    return @certificate
+  end
+end
