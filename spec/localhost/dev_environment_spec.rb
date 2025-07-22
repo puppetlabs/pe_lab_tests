2fdeebab74604171dba89d1c2aebbb3c22d52c4d
Jain Waldrip <jain.waldrip@perforce.com>
2025-07-22 15:15:44 -0500

    Add dev environment spec

---
+++ spec/localhost/dev_environment_spec.rb
@@ -0,0 +1,31 @@
+# Copyright 2025. Puppet, Inc., a Perforce company.  
+
+require 'spec_helper'
+
+describe 'Development Environment node' do
+
+  describe package('code') do
+    it { should be_installed }
+  end
+
+  describe package('pdk') do
+    it { should be_installed }
+  end
+
+  describe package('git') do
+    it { should be_installed }
+  end
+
+  describe package('xrdp') do
+    it { should be_installed }
+  end
+
+  describe service('xrdp') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe file('/etc/xrdp/xrdp.ini') do
+    it { should be_file }
+  end
+end
