b6f3b58453753ddc88d0acd9f8ffdbf31bb35b23
Jain Waldrip <jain.waldrip@perforce.com>
2025-07-22 15:05:28 -0500

    Update PE Primary services in spec

--- spec/localhost/puppet_server_spec.rb
+++ spec/localhost/puppet_server_spec.rb
@@ -4,11 +4,26 @@ require 'spec_helper'
 
 describe 'Puppet Server node' do
 
-  describe package('puppetserver') do
+  describe package('pe-puppetserver') do
     it { should be_installed }
   end
 
-  describe service('puppetserver') do
+  describe service('pe-puppetserver') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe service('puppet') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe service('pe-puppetdb') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe service('pe-console-services') do
     it { should be_enabled }
     it { should be_running }
   end
