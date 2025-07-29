63066c0177e4ad4acc3fb48439bc498b35e2e463
Jain Waldrip <jain.waldrip@perforce.com>
2025-07-29 13:27:31 -0500

    Add ruby installer task

---
+++ tasks/install_ruby.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Get Ruby version from parameter
+RUBY_VERSION=$PT_ruby_version
+echo "Installing Ruby version: $RUBY_VERSION"
+
+# Install dependencies
+sudo dnf config-manager --set-enabled powertools
+sudo dnf groupinstall "Development Tools" -y
+sudo dnf install -y libyaml-devel
+sudo dnf install git curl openssl-devel readline-devel zlib-devel libffi-devel -y
+
+# Install rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+# Install ruby-build
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# Install Ruby (using parameter)
+time ~/.rbenv/bin/rbenv install $RUBY_VERSION
+time ~/.rbenv/bin/rbenv global $RUBY_VERSION
+
+# Install bundler
+time ~/.rbenv/shims/gem install bundler
+time ~/.rbenv/bin/rbenv rehash
+
+# Verify installation
+echo "Ruby version:"
+~/.rbenv/shims/ruby --version
+echo "Bundler version:"
+~/.rbenv/shims/bundle --version
