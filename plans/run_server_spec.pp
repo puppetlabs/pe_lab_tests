fcb20539e5f8329a4a29ebceef539e700178aba2
Jain Waldrip <jain.waldrip@perforce.com>
2025-07-30 13:47:00 -0500

    Add a catch for spec files not ending in .rb

--- plans/run_server_spec.pp
+++ plans/run_server_spec.pp
@@ -60,18 +60,23 @@ plan pe_lab_tests::run_server_spec (
 
   # Verify the spec file exists
   out::message('Verifying spec file exists...')
-  $spec_path = "${project_dest}/pe_lab_tests/spec/localhost/${spec_file}"
+  if ! $spec_file =~ /.+\.rb$/ {
+    spec_file_final = "${spec_file}.rb"
+  } else {
+    spec_file_final = $spec_file
+  }
+  $spec_path = "${project_dest}/pe_lab_tests/spec/localhost/${spec_file_final}"
   $file_check = run_command("test -f ${spec_path} && echo 'exists' || echo 'not found'", $targets)
 
   $file_check.each |$result| {
     if $result.value['stdout'].strip == 'not found' {
-      fail_plan("Spec file ${spec_path} on target ${result.target}")
+      fail_plan("Spec file ${spec_path} not found on target ${result.target}")
     }
   }
 
   # Run the specified spec file
-  out::message("Running spec file: ${spec_file}...")
-  $spec_results = run_command("cd ${project_dest}/pe_lab_tests && ~/.rbenv/shims/bundle exec rspec spec/localhost/${spec_file} --format documentation", $targets, {
+  out::message("Running spec file: ${spec_file_final}...")
+  $spec_results = run_command("cd ${project_dest}/pe_lab_tests && ~/.rbenv/shims/bundle exec rspec spec/localhost/${spec_file_final} --format documentation", $targets, {
       '_run_as' => $user
   })
 
@@ -103,7 +108,7 @@ plan pe_lab_tests::run_server_spec (
 
   return {
     status              => $status,
-    spec_file           => $spec_file,
+    spec_file           => $spec_file_final,
     ruby_version        => $ruby_version,
     project_destination => $project_dest,
     targets_tested      => $targets,
